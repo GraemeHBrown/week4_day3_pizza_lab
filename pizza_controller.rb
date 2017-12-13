d4642088e519268549725a30f3202f29ec93cde9
Graeme Brown <graemehbrown@googlemail.com>
2017-12-13 14:46:12 +0000

    Starter code and setup for Pizza shop lab.

---
+++ pizza_controller.rb
@@ -0,0 +1,37 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./models/pizza')
+
+#index route
+get('/pizzas') do
+  @pizzas = Pizza.all()
+  erb :index
+end
+
+#new action
+get('/pizzas/new') do
+  erb :new
+end
+
+#show
+get('/pizzas/:id') do
+  id = params[:id]
+  @pizza = Pizza.find(id)
+  erb :show
+end
+
+#create
+post('/pizzas')do
+  @pizza = Pizza.new(params)
+  @pizza.save()
+  erb :create
+end
+
+#edit
+get('/pizzas/:id/edit') do
+  id = params[:id]
+  @pizza = Pizza.find(id)
+  erb :edit
+
+end
